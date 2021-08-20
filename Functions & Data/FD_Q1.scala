f4823a151df14db2912db0f28b041c7e05703a62
VenodiW <72806077+VenodiW@users.noreply.github.com>
2021-08-20 23:49:14 +0530

    Update FD_Q1.scala

--- Functions & Data/FD_Q1.scala
+++ Functions & Data/FD_Q1.scala
@@ -1,10 +1,21 @@
+//19001908
 object FD_Q1 extends App {
   class Rational (x:Int,y:Int){
-    def numer = x
-    def denom = y
-    def add(r:Rational) = new Rational(numer * r.denom + r.numer * denom, denom * r.denom)
-    def mul(r:Rational) = new Rational(numer * r.numer, denom * r.denom)
-    def neg(r:Rational) = new Rational(-r.numer, r.denom)
-    println(neg(3,4))
+    require(y > 0)
+
+    def this(x:Int) = this(x,1)
+
+    private def gcd(numer: Int, denom: Int): Int = if (denom == 0) numer else gcd(denom, numer % denom)
+
+    def numer:Int = x / gcd(Math.abs(x), y)
+    def denom:Int = y / gcd(Math.abs(x), y)
+
+    def neg():Rational = new Rational(-this.numer,this.denom)
+
+    override def toString: String = numer  + "/" + denom
   }
+
+  var a = new Rational(3,4)
+  var b = a.neg()
+  println("Negative rational number of ", a, "is: ", b)
 }

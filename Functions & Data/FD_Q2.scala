9ba5332e3b3598f8c19ffde112b370a3233a5c4b
VenodiW <72806077+VenodiW@users.noreply.github.com>
2021-08-20 23:49:56 +0530

    Update FD_Q2.scala

--- Functions & Data/FD_Q2.scala
+++ Functions & Data/FD_Q2.scala
@@ -1,10 +1,18 @@
+//19001908
 object FD_Q2 extends App {
   class Rational (x:Int,y:Int) {
-    def numer = x
-    def denom = y
-    def +(r: Rational) = new Rational(numer * r.denom + r.numer * denom, denom * r.denom)
-    def neg = new Rational(-this.numer, this.denom)
-    def -(r: Rational) = this+r.neg
+    require(y > 0)
+
+    def this(x:Int) = this(x,1)
+
+    private def gcd(numer: Int, denom: Int): Int = if (denom == 0) numer else gcd(denom, numer % denom)
+
+    def numer:Int = x / gcd(Math.abs(x), y)
+    def denom:Int = y / gcd(Math.abs(x), y)
+
+    def -(r: Rational):Rational = new Rational(this.numer * r.denom - this.denom * r.numer, this.denom * r.denom)
+
+    override def toString: String = numer  + "/" + denom
     }
 
   val x = new Rational(3,4)

279a8fe67c0d943dab69ab6f6bd357a8dbd9691c
VenodiW <72806077+VenodiW@users.noreply.github.com>
2021-08-20 23:50:30 +0530

    Update FD_Q3.scala

--- Functions & Data/FD_Q3.scala
+++ Functions & Data/FD_Q3.scala
@@ -1,15 +1,25 @@
+//19001908
 object FD_Q3 extends App {
-  var bank:List[Account]=List()
+
   class Account(id:String, n:Int, b:Double){
-    val nic:String = id
-    val accnumber: Int = n
-    val balance: Double = b
+    var nic:String = id
+    var accnumber: Int = n
+    var balance: Double = b
 
     override def toString = "["+nic+" : "+accnumber+" : "+balance+"]"
 
-    def withdraw(a:Double):Account = this.balance-a
-    def deposit(a:Double):Account = this.balance+a
-    def transfer (a:Account, b:Double) = this.balance-b ; new Account(deposit(balance+b))
+    def withdraw(a:Double)= this.balance-a
 
+    def deposit(a:Double)= this.balance+a
+
+    def transfer (a:Account, b:Double) ={
+      a.balance = a.balance + b
+      this.balance = this.balance - b
+    }
   }
+
+  val user1 = new Account("675460809V" , 1180, 15000)
+  val user2 = new Account("998020980V" , 6023, 7800)
+
+  println(user1.transfer(user2,1000))
 }
